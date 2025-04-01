da7a7bc18f106d10543c8ca236444d5578eb8039
Florian HARADJI <florian.haradji@ariane.group>
2025-04-01 11:21:16 +0200

    #1 done. side effect : checks stack balance on Lua calls

--- Source/LuaBridge/detail/LuaException.h
+++ Source/LuaBridge/detail/LuaException.h
@@ -33,6 +33,55 @@
 #include <string>
 
 namespace luabridge {
+  /// saves stack top in Lua registry to set it cleanly back after pcall.
+  /// stores backtrace on error
+  class LuaCaller {
+  public:
+    /// call this before pushing function and args to call
+    static void preCall(lua_State* L) {
+      // assert(registry[L] == nil) -- check preCall/postCall sequence consistency
+      const int top = ::lua_gettop(L);
+      ::lua_pushlightuserdata(L, L);
+      ::lua_gettable(L, LUA_REGISTRYINDEX);
+      assert(lua_isnil(L, -1) == 1);
+      lua_pop(L, 1);
+       // regitry[L] = stack top -- save stack top to restore
+      ::lua_pushlightuserdata(L, L);
+      ::lua_pushinteger(L, top);
+      ::lua_settable(L, LUA_REGISTRYINDEX);
+      // push backtrace function to reference it in error case
+      ::lua_pushcfunction(L, LuaBacktrace);
+    }
+
+    /// call this after getting the return values from stack
+    static void postCall(lua_State* L) {
+      // assert(type(registry[L]) == "number") -- check preCall/postCall sequence consistency
+      ::lua_pushlightuserdata(L, L);
+      ::lua_gettable(L, LUA_REGISTRYINDEX);
+      assert(lua_isnumber(L, -1) == 1);
+      // top = registry[L]
+      const int top = ::lua_tointeger(L, -1);
+      // registry[L] = nil
+      ::lua_pushlightuserdata(L, L);
+      ::lua_pushnil(L);
+      ::lua_settable(L, LUA_REGISTRYINDEX);
+      // restore stack top
+      ::lua_settop(L, top);
+    }
+
+  private:
+    static int LuaBacktrace(lua_State* L) {
+      // top = registry[L]
+      ::lua_pushlightuserdata(L, L);
+      ::lua_gettable(L, LUA_REGISTRYINDEX);
+      assert(lua_isnumber(L, -1) == 1);
+      const int top = ::lua_tointeger(L, -1);
+      // push errmsg + stack trace
+      ::luaL_traceback(L, L, ::lua_tostring(L, -2), top);
+      return 1;
+    }
+  };
+
 
 class LuaException : public std::exception 
 {
@@ -93,9 +142,9 @@ public:
   /**
       Wrapper for lua_pcall that throws.
   */
-  static void pcall (lua_State* L, int nargs = 0, int nresults = 0, int msgh = 0)
+  static void pcall (lua_State* L, int nargs = 0, int nresults = 0)
   {
-    int code = lua_pcall (L, nargs, nresults, msgh);
+    int code = lua_pcall (L, nargs, nresults, -(nargs + 2)); // error handler is pushed before function and args
 
     if (code != LUABRIDGE_LUA_OK)
       Throw (LuaException (L, code));
@@ -117,13 +166,13 @@ protected:
     {
       char const* s = lua_tostring (m_L, -1);
       m_what = s ? s : "";
-      lua_pop(m_L,1);
     }
     else
     {
       // stack is empty
       m_what = "missing error";
     }
+    LuaCaller::postCall(m_L);
   }
 
 private:
